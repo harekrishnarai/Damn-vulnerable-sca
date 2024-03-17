a6102028d9afaded90e4b85b1b4c06679917a9d8
Nandan Gupta <nandangupta@Nandans-MBP.attlocal.net>
2024-03-16 22:41:24 -0700

    Added Vulnerable Log4j

--- backend/src/main/java/com/acme/foo/PersonApi.java
+++ backend/src/main/java/com/acme/foo/PersonApi.java
@@ -2,9 +2,8 @@ package com.acme.foo;
 
 import java.util.Calendar;
 import java.util.Date;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/persons")
 public class PersonApi {
 
-	private static Logger log = LoggerFactory.getLogger(PersonApi.class);
+	private static final Logger logger = LogManager.getLogger(PersonApi.class);
 
 	@Autowired
 	PersonApi() {
@@ -34,7 +33,7 @@ public class PersonApi {
 			p.setId(1);
 			p.setFirstname("John");
 			p.setLastname("Doe");
-
+			logger.info("Info log get Nandan");
 			final Date d = Calendar.getInstance().getTime();
 			p.setCreatedAt(d);
 			p.setModifiedAt(d);
@@ -48,7 +47,8 @@ public class PersonApi {
 			"application/json;charset=UTF-8" }, produces = { "application/json;charset=UTF-8" })
 	public ResponseEntity<Person> postPerson(@RequestBody Person person) {
 		try {
-			System.out.println("Upload of person [" + person + "]");
+			//System.out.println("Upload of person [" + person + "]");
+			logger.info("Info log Nandan"+person.firstname);
 			return new ResponseEntity<Person>(person, HttpStatus.OK);
 		} catch (Exception enfe) {
 			return new ResponseEntity<Person>(HttpStatus.NOT_FOUND);
